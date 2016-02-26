79261ba91fa3304d3138b8513be38e304682962c
brandondoran <bdoran@gmail.com>
2016-02-25 21:03:09 -0800

    Seperate bundles for app and vendor

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -4,19 +4,25 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   devtool: 'source-map',
-  entry: [
-    './app/index'
-  ],
+  entry: {
+    app: './app/index',
+    vendor: ['react', 'react-dom']
+  },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle-[hash].js',
-    publicPath: 'static/'
+    publicPath: 'static/',
+    filename: '[name].[chunkhash].js',
+    chunkFilename: '[name].[chunkhash].js'
   },
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(true),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: Infinity
+    }),
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+        NODE_ENV: '"production"'
       }
     }),
     new webpack.optimize.UglifyJsPlugin({
