5ff492da087846c5f66ce0048b9ff3648927c51e
brandondoran <bdoran@gmail.com>
2016-05-16 22:25:11 -0700

    Upgrade react, webpack, react-hot-loader 3 (beta)

--- app/index.js
+++ app/index.js
@@ -1,5 +1,22 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
 import App from './components/App';
 
-render(<App />, document.getElementById('root'));
+render(
+  <AppContainer>
+    <App />
+  </AppContainer>,
+  document.getElementById('root'));
+
+if (module.hot) {
+  module.hot.accept('./components/App', () => {
+    const NextApp = require('./components/App').default;
+    render(
+      <AppContainer>
+        <NextApp />
+      </AppContainer>,
+      document.getElementById('root')
+    );
+  });
+}
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -4,7 +4,7 @@ const webpack = require('webpack');
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: [
-    'eventsource-polyfill', // necessary for hot reloading with IE
+    'react-hot-loader/patch',
     'webpack-hot-middleware/client',
     './app/index'
   ],
