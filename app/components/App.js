83c790c731b9f968d153a748ecad6a2679b8232b
Brandon Doran <bdoran@gmail.com>
2016-06-11 23:13:46 -0700

    Add react-router (#3)

--- app/components/App.js
+++ app/components/App.js
@@ -1,5 +1,22 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import { Link } from 'react-router';
 
-const App = () => <div>react-webpack-starter!</div>;
+const App = ({ children }) => (
+  <div>
+    <header>
+      {' '}
+      <Link to="/">Home</Link>
+      {' '}
+      <Link to="/about">About</Link>
+      {' '}
+    </header>
+    <div style={{ marginTop: '1.5em' }}>{children}</div>
+  </div>
+);
+
+
+App.propTypes = {
+  children: PropTypes.object.isRequired
+};
 
 export default App;
